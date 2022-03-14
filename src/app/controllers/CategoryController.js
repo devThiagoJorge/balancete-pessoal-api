0cf68408771369270e24a4bf028e4f6c2a485a22
Thiago Jorge <thiago.v.jorge@hotmail.com>
2022-03-14 16:15:42 -0300

    finish category controller

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -1,26 +1,67 @@
 const Category = require('../models/Category');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
+
 
 module.exports = {
-    async index(req, res) {
-        return res.send({
-            chave: "Olá"
-        });
+    async findAll(req, res) {
+        try {
+            const categories = await Category.findAll();
+            return res.json(categories);
+        } catch (error) {
+            return console.error("Error: ", error);
+        }
     },
+
+
 
     async create(req, res) {
         const {
             name,
         } = req.body;
 
+        const findCategory = await findByName(name);
+
+        if (findCategory != null) {
+            return res.status(400).json({
+                error: 'Category already exists.'
+            });
+        }
+
         try {
             const category = await Category.create({
                 name,
-                createdAt: Date.now(),
-                updatedAt: Date.now()
             });
             return res.json(category);
         } catch (error) {
             return console.error('Error: ', error);
         }
     },
+
+    async delete(req, res) {
+        try {
+            await Category.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+
+            return res.json({
+                msg: `Category ${req.params.id} was deleted!`
+            });
+        } catch (error) {
+            return console.error("Error: ", error);
+        }
+    },
 };
+
+async function findByName(name) {
+    console.log(name);
+    const category = await Category.findOne({
+        where: {
+            name: name
+        }
+    });
+    console.log(category);
+    return category
+}
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 const categoryController = require('./app/controllers/CategoryController');
 
 
-routes.get('/categories', categoryController.index);
+routes.get('/categories', categoryController.findAll);
 routes.post('/categories', categoryController.create);
-
+routes.delete('/categories/:id', categoryController.delete)
 module.exports = routes;
