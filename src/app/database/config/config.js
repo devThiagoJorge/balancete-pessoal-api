4515da23282af54a5abc915dce12ff27cc0f893b
Thiago Jorge <thiago.v.jorge@hotmail.com>
2022-03-17 17:02:56 -0300

    Change production config

--- src/app/database/config/config.js
+++ src/app/database/config/config.js
@@ -20,11 +20,11 @@ module.exports = {
     logging: true
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    use_env_variable: DATABASE_URL,
+    dialect: postgres,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 };
