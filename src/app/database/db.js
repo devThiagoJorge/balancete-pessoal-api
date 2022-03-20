9d7b369563d992dd78c758b63182bb28e54537a4
Thiago Jorge <thiago.v.jorge@hotmail.com>
2022-03-20 13:49:08 -0300

    added procfile and change db configs

--- src/app/database/db.js
+++ src/app/database/db.js
@@ -1,14 +1,35 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    define: {
-        underscored: true,
-        createdAt: 'created_at',
-        updatedAt: 'updated_at',
-    }
-});
+var sequelize;
+
+if (process.env.NODE_ENV === "production") {
+    var sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+            }
+        },
+        define: {
+            underscored: true,
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        }
+    });
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        define: {
+            underscored: true,
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        }
+    });
+}
+
+
 
 module.exports = sequelize;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 
 //const requireDir = require('require-dir');
@@ -14,6 +16,6 @@ app.use(express.json());
 
 app.use('/', require('./routes'));
 
-app.listen(process.env.PORT | 3001);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 console.log('Server is running!');
